ccb6b4a7659f15fdca50277a5c719b9c06cda72f
LetsMelon <domi.m@outlook.com>
2023-08-24 20:34:38 +0200

    fix html translation from 'LineBreak'

--- src/html/line_break.rs
+++ src/html/line_break.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 
 use anyhow::Result;
 
-use crate::html::IntoHtmlNode;
+use crate::html::{IntoHtmlNode, IsParagraph};
 
 #[derive(Debug)]
 pub struct LineBreak;
@@ -15,8 +15,22 @@ impl LineBreak {
 
 impl IntoHtmlNode for LineBreak {
     fn transform_into_html_node(&self, buffer: &mut dyn Write) -> Result<()> {
-        write!(buffer, "</br>")?;
+        write!(buffer, "<br />")?;
 
         Ok(())
+    }
+}
+
+// TODO I'm not quit sure if `LineBreak` implements the trait `IsParagraph`.
+// But at the moment this is the only way to use `Hyperlink` in a `Paragraph`
+// Maybe there is another, and better way, to do this?
+// same as `Image`, `Hyperlink`
+impl IsParagraph for LineBreak {
+    fn to_raw(&self) -> String {
+        let mut vec = Vec::new();
+
+        self.transform_into_html_node(&mut vec).unwrap();
+
+        String::from_utf8(vec).unwrap()
     }
 }
