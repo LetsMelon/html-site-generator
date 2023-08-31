52db2a6637bc501ce0621c61ac284712dccbd9ed
LetsMelon <domi.m@outlook.com>
2023-08-31 16:42:58 +0200

    add parsing test to 'document'

--- src/html/document.rs
+++ src/html/document.rs
@@ -27,3 +27,41 @@ impl IntoHtmlNode for Document {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Document;
+    use crate::attributes::SetHtmlAttributes;
+    use crate::html::body::Body;
+    use crate::html::div::Div;
+    use crate::html::head::Head;
+    use crate::html::meta::Meta;
+    use crate::html::test_harness::test_generates_correct_html;
+    use crate::html::title::Title;
+
+    test_generates_correct_html!(generates_correct_html_empty, {
+        Document::new(Head::new(), Body::new())
+    });
+
+    test_generates_correct_html!({
+        let mut head = Head::new();
+        head.add_element(Title::new("test_page"));
+        head.add_element({
+            let mut m = Meta::new();
+            m.add_pair("CHARSET", "UTF-8");
+            m
+        });
+
+        let mut body = Body::new();
+        body.add_element({
+            let mut d = Div::new();
+            d.add_class("test_div");
+
+            d.add_element("Hello, I'm a text inside a div inside a document inside a test");
+
+            d
+        });
+
+        Document::new(head, body)
+    });
+}
